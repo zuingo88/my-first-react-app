2c4df2218b620955e4e7a6599b6a41bd012978be
valerio sgura <valeriosgura88@gmail.com>
2021-09-09 17:03:59 +0200

    Primo componente.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,7 +1,10 @@
+import Componente1 from './Componente1'
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Componente1 />
     </div>
   );
 }
---
+++ first-react/src/Componente1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Componente1() {
+  return (
+    <div>
+      <h3>Il mio primo Componente</h3>
+    </div>
+  )
+}
+
+export default Componente1
