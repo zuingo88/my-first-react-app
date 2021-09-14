8c4e38eb55f3b25ef9130b009f5e9370534529a2
valerio sgura <valeriosgura88@gmail.com>
2021-09-14 12:04:38 +0200

    Props.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -2,21 +2,64 @@ import Componente1 from './Componente1'
 import Componente2 from './Componente2'
 import Product from './Product'
 
+const primaCard = {
+  nomeProdotto: 'primoNome',
+  prezzoProdotto: '1',
+  immagine: 'prima immagine',
+  disponibilita: 'disponibile'
+}
+
+const secondaCard = {
+  nomeProdotto: 'secondoNome',
+  prezzoProdotto: '2',
+  immagine: 'seconda immagine',
+  disponibilita: 'non disponibile'
+}
+
+const pStyle = {
+  textTransform: 'uppercase'
+}
+
+// const Product = (props) => {
+
+//   console.log(props);
+
+//   return (
+//     <div className='card'>
+//       <h4>{props.nome}</h4>
+//       <h5>{props.prezzo} €</h5>
+//       <div className="preview">{props.immagine}</div>
+//       <p style={pStyle}>{props.disponibilita}</p>
+//       <p>{'LOWERCASE'.toLowerCase()}</p>
+//       <button>Acquista</button>
+//     </div>
+//   )
+// }
+
 function App() {
   return (
     <div className="App">
+
       <h1>Hello World!</h1>
       <Componente1 />
       <Componente2 />
 
       <section className='card-container'>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+
+        <Product
+          nome={primaCard.nomeProdotto}
+          prezzo={primaCard.prezzoProdotto}
+          immagine={primaCard.immagine}
+          disponibilita={primaCard.disponibilita}
+        />
+
+        <Product
+          nome={secondaCard.nomeProdotto}
+          prezzo={secondaCard.prezzoProdotto}
+          immagine={secondaCard.immagine}
+          disponibilita={secondaCard.disponibilita}
+        />
+
       </section>
     </div>
   );
--- first-react/src/Product.js
+++ first-react/src/Product.js
@@ -1,11 +1,36 @@
 import React from 'react'
 
-const Product = () => {
+const pStyle = {
+  textTransform: 'uppercase'
+}
+
+// const Product = (props) => {
+
+//   console.log(props);
+
+//   return (
+//     <div className='card'>
+//       <h4>Prodotto</h4>
+//       <h5>Prezzo</h5>
+//       <div className="preview">Img</div>
+//       <p style={pStyle}>Disponibile</p>
+//       <p>{'LOWERCASE'.toLowerCase()}</p>
+//       <button>Acquista</button>
+//     </div>
+//   )
+// }
+
+const Product = (props) => {
+
+  console.log(props);
+
   return (
     <div className='card'>
-      <h4>Prodotto</h4>
-      <h5>Prezzo</h5>
-      <div className="preview">Img</div>
+      <h4>{props.nome}</h4>
+      <h5>{props.prezzo} €</h5>
+      <div className="preview">{props.immagine}</div>
+      <p style={pStyle}>{props.disponibilita}</p>
+      <p>{'LOWERCASE'.toLowerCase()}</p>
       <button>Acquista</button>
     </div>
   )
