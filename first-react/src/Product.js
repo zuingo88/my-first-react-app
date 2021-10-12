5fcd5c5e8b177f733a20b846597016ee086ae9d9
valerio sgura <valeriosgura88@gmail.com>
2021-10-12 17:23:13 +0200

    Gestione degli eventi.

--- first-react/src/Product.js
+++ first-react/src/Product.js
@@ -14,16 +14,34 @@ const pStyle = {
 
 const Product = ({ nomeProdotto, prezzoProdotto, immagine, disponibilita, children }) => {
 
+  const handleClick = () => {
+    console.log("Hai premuto su un'immagine");
+  };
+
+  const paramsHandler = (prezzoProdotto) => {
+    console.log(prezzoProdotto);
+  };
+
+  const eventHandler = (prezzo) => (testo) => {
+    console.log(testo.target.innerText);
+    console.log(prezzo);
+  };
+
+  const saluto = () => {
+    console.log(`Ciao!`);
+  }
 
   return (
-    <div className='card'>
+    <div className='card' onMouseEnter={saluto}>
       <h4>{nomeProdotto}</h4>
       <h5>{prezzoProdotto} €</h5>
-      <div className="preview">{immagine}</div>
+      <div className="preview" onClick={handleClick}>{immagine}</div>
       <p>{children}</p>
       <p style={pStyle}>{disponibilita}</p>
       <p>{'LOWERCASE'.toLowerCase()}</p>
-      <button>Acquista</button>
+      <button onClick={() => alert(`Hai premuto ${nomeProdotto}`)}>Acquista</button>
+      <button onClick={() => paramsHandler(prezzoProdotto)}>console.log del prezzo</button>
+      <button onClick={eventHandler(prezzoProdotto)}>log di questo testo e prezzo</button>
     </div>
   )
 }
